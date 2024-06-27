baf05c12a636efacde3ba951279fbf40cf9861d3
Daniel Bixler <danielbixler@yahoo.com>
2024-06-27 19:02:29 -0400

    Add project files.

---
+++ NorthWind_CS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NorthWind_CS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
