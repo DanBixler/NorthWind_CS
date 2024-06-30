30c135a047ef510ed6360e961f6bd0bd7ba17bd1
Daniel Bixler <danielbixler@yahoo.com>
2024-06-29 21:29:55 -0400

    Added Northwind logo image file, scaffolded models from the database, started AppDBContext file.

---
+++ NorthWind_CS/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthWind_CS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+
+
+}
