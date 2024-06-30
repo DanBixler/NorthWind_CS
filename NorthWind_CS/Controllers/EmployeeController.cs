4293d705642ce52739a8d9bc561a98acc730011e
Daniel Bixler <danielbixler@yahoo.com>
2024-06-30 18:09:16 -0400

    Added Employee page

---
+++ NorthWind_CS/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthWind_CS.Models;
+using System.Linq;
+
+namespace NorthWind_CS.Controllers
+{
+    public class EmployeeController : Controller
+    {        
+        public IActionResult Employees()
+        {
+            return View();
+        }
+    }
+}
--- NorthWind_CS/Controllers/HomeController.cs
+++ NorthWind_CS/Controllers/HomeController.cs
@@ -1,26 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using NorthWind_CS.Models;
 using System.Diagnostics;
+using System.Linq;
 
 namespace NorthWind_CS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NorthwindContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _context = new NorthwindContext();
         }
 
         public IActionResult Index()
-        {
+        {            
             return View();
         }
 
         public IActionResult Employees()
         {
-            return View();
+            var employees = _context.Employees.ToList();
+            return View(employees);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
